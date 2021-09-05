9810303e85f0f56368a4a49c8d61a034931565ad
pawar4 <pawar4@purdue.edu>
2021-09-05 12:58:13 -0400

    update sql schema

--- ritoman_betting_service/internal/schema.sql
+++ ritoman_betting_service/internal/schema.sql
@@ -2,3 +2,11 @@
 \c rbsdb;
 
 -- TODO create table definitions here
+CREATE TABLE IF NOT EXISTS active_bets (
+    game_id INT,
+    better_id VARCHAR,
+    bettee_id VARCHAR,
+    bet_condtion VARCHAR,
+    bet_amount INT, 
+    PRIMARY KEY (game_id, better_id, bettee_id)
+)
