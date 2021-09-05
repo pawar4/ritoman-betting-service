6b3ebe6cd2f39ab473ecb5d766599f990df1188e
pawar4 <pawar4@purdue.edu>
2021-09-05 12:51:34 -0400

    add create bet functionality

--- ritoman_betting_service/internal/database.py
+++ ritoman_betting_service/internal/database.py
@@ -1,1 +1,31 @@
 # TODO implement database accessor here
+from contextlib import contextmanager
+import psycopg2
+from psycopg2.extras import DictCursor
+
+DATABASE_NAME = ''
+
+@contextmanager
+def session():
+    conn = psycopg2.connect(
+        database=DATABASE_NAME,
+        host='postgresql',
+        user='docker',
+        port=5432,
+        cursor_factory=DictCursor
+    )
+    conn.set_session(autocommit=True)
+    curr = conn.cursor()
+    yield curr
+    curr.close()
+    conn.close()
+
+def register_bet(game_id: int, better_id: str, bettee_id: str, bet_condition: str, bet_amount: int):
+
+    with session() as db:
+        try:
+            db.execute(
+                'INSERT INTO active_bets (game_id, better_id, bettee_id, bet_condtion, bet_amount)\
+                    VALUES (%(game_id)d, %(better_id)s, %(bettee_id)s, %(bet_condtion)s, %(bet_amount)d)',
+                {'game_id' : game_id, 'better_id': better_id, 'bettee_id': bettee_id, 'bet_condition': bet_condition, 'bet_amount': bet_amount}
+            )
--- ritoman_betting_service/routers/listener.py
+++ ritoman_betting_service/routers/listener.py
@@ -1,9 +1,29 @@
 # router for all endpoints that are triggered via webhooks
 # see webhook-router for more details
-from fastapi import APIRouter
+from ritoman_betting_service.internal.database import register_bet
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix='/listeners',
     tags=['listeners'],
     dependencies=[],
 )
+
+class Bet(BaseModel):
+    game_id : int
+    better_id : str
+    bettee_id : str
+    bet_condition : str
+    bet_amount : int
+
+
+@router.post('/')
+async def create_bet(bet : Bet):
+    #add to database
+    result = register_bet(**bet.dict())
+    #handle database response
+    if not result:
+        raise HTTPException(400)
+    #send response message
+    return True
